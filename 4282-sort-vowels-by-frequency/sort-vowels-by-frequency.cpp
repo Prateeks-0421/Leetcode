523c9be2e2395a740b90e769a922b61657dd2eb8
Prateeks-0421 <peekingsurfers01@gmail.com>
2026-05-03 02:45:58 +0530

    Time: 8 ms (92.21%) | Memory: 16.8 MB (97.79%) - LeetSync

--- 4282-sort-vowels-by-frequency/sort-vowels-by-frequency.cpp
+++ 4282-sort-vowels-by-frequency/sort-vowels-by-frequency.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
-    bool isVowel(char ch){
-        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
-    }
     string sortVowels(string s) {
-        unordered_map<char,int> m;
-        for(char ch:s){
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                m[ch]++;
-            }
-        }
-        vector<pair<char,int>> v;
-        for(auto &p:m){
-            v.push_back(p);
-        }
-        sort(v.begin(),v.end(),[](auto &a,auto &b){
-            return a.second<b.second;
-        });
-        stack<char> st;
-        for(int i=0;i<v.size();i++){
-            while(v[i].second--)
-            st.push(v[i].first);
-        }
-        for(int i=0;i<s.size();i++){
-            if(isVowel(s[i])){
-                char ch=st.top();
-                s[i]=ch;
-                st.pop();
-            }
-        }
-        return s;
+        
+     unordered_map<char,int> freq ; 
+
+     for(int i = 0 ; i < s.size() ; i++ ){
+
+
+        if( s[i] == 'a' or s[i] == 'e' or s[i] == 'i' or s[i] == 'o' or s[i] == 'u' )
+        freq[s[i]]++ ; 
+
+     }
+    
+    vector<pair<char, int>> vec( freq.begin(), freq.end());
+
+      sort(vec.begin(), vec.end(), [](const auto& a, const auto& b) {
+   
+        return a.second < b.second; 
+         
+    });
+    
+   
+   int k = vec.size() - 1 ; 
+
+    for(int i = 0 ; i  < s.size() ; i++ ){
+
+      if( s[i] == 'a' or s[i] == 'e' or s[i] == 'i' or s[i] == 'o' or s[i] == 'u' ){
+
+        s[i] = vec[k].first ;
+
+        vec[k].second-- ; 
+        if( vec[k].second == 0 ) k-- ; 
+
+      }
+
+    }
+    return s ; 
+
     }
 };
