ad4d82cc207eabf89f2841a41292122249f8e9ac
Prateeks-0421 <peekingsurfers01@gmail.com>
2026-07-30 17:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (82.27%) - LeetSync

---
+++ 2748-calculate-delayed-arrival-time/calculate-delayed-arrival-time.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        
+    int ans = arrivalTime + delayedTime ; 
+
+    return ans % 24 ; 
+
+
+    }
+};
